9e8876e97ef4e147d384ee3f4fc97389f5df4186
wild._b <goodboy1tryit@gmail.com>
2017-12-25 00:38:16 +0800

    project init

---
+++ README.md
@@ -0,0 +1,25 @@
+## 个人理财移动端项目 Vue.js（一）
+
+毕业设计：个人理财
+# AccountPortal
+
+## 模板文档
+[使用文档](https://github.com/aOrz/vue-template-for-hbuilder/edit/master/README.md)
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# 自动编译结果到指定文件夹
+npm run dev
+
+# serve with hot reload at localhost:8080
+npm run webdev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view></router-view>-->
+    <button type="button" class="mui-btn">默认</button>
+    <button type="button" class="mui-btn mui-btn-primary">蓝色</button>
+    <button type="button" class="mui-btn mui-btn-success">绿色</button>
+    <button type="button" class="mui-btn mui-btn-warning">黄色</button>
+    <button type="button" class="mui-btn mui-btn-danger">红色</button>
+    <button type="button" class="mui-btn mui-btn-royal">紫色</button>
+
+  </div>
+</template>
+
+<script>
+import plus from 'plus' // use plus
+import mui from 'mui' // use mui
+mui.init();
+const a = 0;
+console.log(a);
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
